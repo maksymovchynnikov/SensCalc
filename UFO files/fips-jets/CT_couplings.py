11ec57d7f1072555c621b63f48c6b2e66b52e2c2
maksymovchynnikov <111570615+maksymovchynnikov@users.noreply.github.com>
2023-10-01 14:09:07 +0200

    v. 1.0.5

---
+++ UFO files/fips-jets/CT_couplings.py
@@ -0,0 +1,11 @@
+# This file was automatically created by FeynRules 2.3.43
+# Mathematica version: 12.1.0 for Microsoft Windows (64-bit) (March 18, 2020)
+# Date: Mon 18 Sep 2023 17:44:46
+
+
+from object_library import all_couplings, Coupling
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+
+
+
